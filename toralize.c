2285d45846dcaa77e4475cc790b919017cb0e061
Silver595 <akashpurjalkar66@gmail.com>
2025-12-18 16:21:48 +0530

    SOCKS impl

--- toralize.c
+++ toralize.c
@@ -4,4 +4,13 @@
 
 int main(int argc,char *argv[]){
     char *host;
+    int port;
+
+    if (argc < 3){
+        fprintf(stderr,"Usage: %s <host> <port>\n",argv[0]);
+        return -1;
+
+    }
+    host = argv[1];
+    port = atoi(argv[2]);
 }
